d017b07d5019e74157083b6bdffe154c417568e6
Bruno Barros <bruno.barros@itixti.com.br>
2019-11-17 21:07:51 -0300

    getting token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +24,18 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC71y7alXjKY9JuFIf0AYlDKH8YYUbIpHw', {
+            email,
+            password,
+            returnSecureToken: true
+        })
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
     };
 };
