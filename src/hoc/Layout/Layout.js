d710570fc63145f07679ec0cf89e499512dbea7f
Bruno Barros <bruno.barros@itixti.com.br>
2020-02-26 13:52:36 -0300

    adding logout link

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
+import { connect } from 'react-redux';
 
 class Layout extends Component {
     state = {
@@ -21,8 +22,10 @@ class Layout extends Component {
     render() {
         return (
             <>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <Toolbar isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer isAuth={this.props.isAuthenticated}
+                    open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -31,4 +34,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
